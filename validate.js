d7514394c07dcb4d7ae585f4a9b4f86aa705646e
naugtur <naugtur@gmail.com>
2025-03-18 15:00:18 +0100

    early draft

---
+++ validate.js
@@ -0,0 +1,18 @@
+const Ajv = require("ajv");
+const fs = require("fs");
+
+const schema = require("./attestation.schema.json");
+const exampleData = require("./example.json");
+
+const ajv = new Ajv();
+
+const validate = ajv.compile(schema);
+
+const valid = validate(exampleData);
+
+if (valid) {
+  console.log("Validation successful. The example JSON is valid.");
+} else {
+  console.log("Validation failed. Errors:");
+  console.dir(validate.errors, { depth: null });
+}
